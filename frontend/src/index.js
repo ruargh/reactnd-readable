380a6487c88b9a5fd1bfb701dc503eed1d908c46
Ruargh <ruargh@gmail.com>
2018-04-26 06:52:26 +0200

    test: Add categories to Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
-import reducer from './reducers'
+import store from './store'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(reducer)
-
-console.log(store.getState)
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'))
 registerServiceWorker();
